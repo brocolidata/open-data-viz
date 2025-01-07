93145027b284246af609eeedad3846d3532bb899
amirbtb <76230761+amirbtb@users.noreply.github.com>
2025-01-07 18:16:53 +0100

    feat: basic light/dark switch

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,6 +5,11 @@
 	import { onMount } from 'svelte'
 	import { loadData } from "$lib/duckdb"
 
+	import Sun from "lucide-svelte/icons/sun";
+	import Moon from "lucide-svelte/icons/moon";
+
+	import { toggleMode } from "mode-watcher";
+
 	let tableName = $state();
 	let renderedData = $state(false);
 
@@ -21,6 +26,16 @@
 	});
 
 </script>
+
+<Button onclick={toggleMode} variant="outline" size="icon">
+	<Sun
+	  class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+	/>
+	<Moon
+	  class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+	/>
+	<span class="sr-only">Toggle theme</span>
+</Button>
 
 <h1>Duck DB Svelte-kit demo</h1>
 
